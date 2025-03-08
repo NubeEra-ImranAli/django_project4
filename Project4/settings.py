72200e791b508417482a8656e47feb691c1b3b6e
NubeEra-ImranAli <nubeera.imranali@gmail.com>
2025-03-08 10:15:07 +0530

    ALLOWED_HOSTS

--- Project4/settings.py
+++ Project4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z%!(3mul)5#uvyhbg@m@6@^7t5)wr11f3w(q$^swgglto*d&o-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
